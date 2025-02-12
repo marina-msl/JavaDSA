15fec14de043ced9d71a6bdaf3fc2b6f2964b3d3
Marina Leide <marina.msleide@gmail.com>
2025-02-12 18:51:41 -0300

    Building a LinkedList

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,31 @@
+package LinkedList;
+
+public class LinkedList {
+	
+	private Node head;
+	private Node tailf;
+	private int lenght;
+	
+	class Node {
+		int value;
+		Node next;
+		
+		public Node(int value) {
+			this.value = value;
+		}
+		
+	}
+
+//	public LinkedList(int value) {
+//		Node newNode = new Node(value);
+//		this.head = newNode;
+//		this.tailf = newNode;
+//		lenght = 1;
+//		
+//	}
+	
+//	public void append(int value) {}
+//	public void prepend(int value) {}
+//	public boolean insert(int value, int index) {}
+
+}
