5a8e7d16e387d700b453a4dfcd3ce7fc28bb5635
Marina Leide <marina.msleide@gmail.com>
2025-02-13 18:03:28 -0300

    Constructor of a LinkedLink and print method

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -16,13 +16,21 @@ public class LinkedList {
 		
 	}
 
-//	public LinkedList(int value) {
-//		Node newNode = new Node(value);
-//		this.head = newNode;
-//		this.tailf = newNode;
-//		lenght = 1;
-//		
-//	}
+	public LinkedList(int value) {
+		Node newNode = new Node(value);
+		this.head = newNode;
+		this.tailf = newNode;
+		lenght = 1;
+	}
+	
+	public void printList() {
+		Node temp = head;
+		
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
 	
 //	public void append(int value) {}
 //	public void prepend(int value) {}
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,5 +1,10 @@
 package LinkedList;
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		LinkedList myLinkedList = new LinkedList(4);
+		
+		myLinkedList.printList();
+	}
 }
