4068fcac2b92d2965549bd73a09dfad544b120bd
Marina Leide <marina.msleide@gmail.com>
2025-02-13 18:01:07 -0300

    LinkedList constructor and print it

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -16,7 +16,6 @@ public class LinkedList {
 		
 	}
 
-<<<<<<< HEAD
 	public LinkedList(int value) {
 		Node newNode = new Node(value);
 		this.head = newNode;
@@ -32,15 +31,6 @@ public class LinkedList {
 			temp = temp.next;
 		}
 	}
-=======
-//	public LinkedList(int value) {
-//		Node newNode = new Node(value);
-//		this.head = newNode;
-//		this.tailf = newNode;
-//		lenght = 1;
-//		
-//	}
->>>>>>> 15fec14de043ced9d71a6bdaf3fc2b6f2964b3d3
 	
 //	public void append(int value) {}
 //	public void prepend(int value) {}
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,14 +1,10 @@
 package LinkedList;
 
 public class Main {
-<<<<<<< HEAD
 	
 	public static void main(String[] args) {
 		LinkedList myLinkedList = new LinkedList(4);
 		
 		myLinkedList.printList();
 	}
-=======
->>>>>>> 15fec14de043ced9d71a6bdaf3fc2b6f2964b3d3
-
 }
