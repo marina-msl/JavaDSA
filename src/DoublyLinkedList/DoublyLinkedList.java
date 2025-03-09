7331236f6ba7c0389b5a2d5dabfef3bcda12ad9e
Marina Leide <marina.msleide@gmail.com>
2025-03-09 13:18:12 -0300

    Improving performance in get() method for the DoublyLinkedList

--- src/DoublyLinkedList/DoublyLinkedList.java
+++ src/DoublyLinkedList/DoublyLinkedList.java
@@ -109,15 +109,24 @@ public class DoublyLinkedList {
 	}
 	
 	public Node get(int index) {
-		
 		if (index < 0 || index >= length ) return null;
 		
 		Node temp = head;
-		
-		for(int i = 0 ; i < index; i ++) {
-			temp = temp.next;
+		if (index < length/2) {
+			for(int i = 0 ; i < index; i ++) {
+				temp = temp.next;
+			}
+		} else {
+			temp = tail;
+			for (int i = length - 1; i > index; i--) {
+				temp = temp.prev;
+			}
 		}
-		
 		return temp;
 	}
+	
+	
+//	public void set(int index, int value) {
+		
+//	}
 }
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -4,14 +4,34 @@ public class Main {
 	
 	public static void main(String[] args) {
 		DoublyLinkedList ddl = new DoublyLinkedList(2);
-		
 		ddl.append(4);
 		ddl.append(6);
-		
+		ddl.append(8);
+		ddl.append(10);
+		ddl.append(12);
+		ddl.append(14);
+		ddl.append(16);
+		ddl.append(18);
+		ddl.append(20);
+
 		System.out.println("Printing LinkedList");
 		ddl.printList();
 
-		System.out.println("Getting Node by index " + ddl.get(3));
+		long now = System.currentTimeMillis();
+		System.out.println("Getting Node by index 0 : " + ddl.get(0).value + " value");
+		long after = System.currentTimeMillis();
+		System.out.println("Time execution " + (after - now));
+		
+		long now1 = System.currentTimeMillis();
+		System.out.println("Getting Node by index 4 : " + ddl.get(4).value + " value");
+		long after1 = System.currentTimeMillis();
+		System.out.println("Time execution " + (after1 - now1));
+		
+		
+		long now2 = System.currentTimeMillis();
+		System.out.println("Getting Node by index 8 : " + ddl.get(8).value + " value");
+		long after2 = System.currentTimeMillis();
+		System.out.println("Time execution " + (after2 - now2));
 		
 	}
 }
