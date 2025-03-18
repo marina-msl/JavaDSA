ddebc87de86da664251f7cf1ef563b92c9c52152
Marina Leide <marina.msleide@gmail.com>
2025-03-17 21:11:10 -0300

    Implementing insert() methond in Binary Search Tree

--- src/BinarySearchTree/BST.java
+++ src/BinarySearchTree/BST.java
@@ -2,7 +2,7 @@ package BinarySearchTree;
 
 public class BST {
 	
-	private Node root;
+	Node root;
 	
 	class Node {
 		int value;
@@ -11,6 +11,35 @@ public class BST {
 		
 		public Node(int value) {
 			this.value = value;
+		}
+	}
+	
+	public boolean insert(int value) {
+		Node newNode = new Node(value);
+		
+		if (root == null) {
+			root = newNode;
+			return true;
+		}
+		
+		Node temp = root;
+		
+		while (true) {
+			if (newNode.value == temp.value) return false;
+			
+			if (newNode.value < temp.value) {
+				if (temp.left == null) {
+					temp.left = newNode;
+					return true;
+				} 
+				temp = temp.left;
+			} else {
+				if (temp.right == null) {
+					temp.right = newNode;
+					return true;
+				}
+				temp = temp.right;
+			}
 		}
 	}
 	
--- src/BinarySearchTree/Main.java
+++ src/BinarySearchTree/Main.java
@@ -5,5 +5,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		BST myBst = new BST();
+		
+		myBst.insert(47);
+		myBst.insert(21);
+		myBst.insert(76);
+		myBst.insert(18);
+		myBst.insert(52);
+		myBst.insert(82);
+		
+		myBst.insert(27);
+		
+		System.out.println(myBst.root.left.right.value);
 	}
 }
