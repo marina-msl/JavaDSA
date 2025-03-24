faa9a9b953b18df6e6caaa99fe96c067872579f0
Marina Leide <marina.msleide@gmail.com>
2025-03-24 20:53:28 -0300

    Implementing set() method in the HashTable class

--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -6,6 +6,18 @@ public class Main {
 		
 		HashTable myHashTable = new HashTable();
 		
+		System.out.println("Printing HashTable");
+		myHashTable.printTable();
+		
+		myHashTable.set("nails", 100);
+		
+		myHashTable.set("tile", 1900);
+		myHashTable.set("lumber", 80);
+
+		myHashTable.set("bolts", 200);
+		myHashTable.set("screws", 140);
+		
+		System.out.println("Printing HashTable");
 		myHashTable.printTable();
 	}
 }
