654cbfa5b9ccb03330d17a96cfbf4974d8488088
Marina Leide <marina.msleide@gmail.com>
2025-04-01 17:07:43 -0300

    Implementing addEdge() method

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -6,8 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		Graph myGraph = new Graph();
 		myGraph.addVertex("A");
+		myGraph.addVertex("B");
+		myGraph.addVertex("C");
+
 		
 		myGraph.printGraph();
+		
+		myGraph.addEdge("A", "B");
+		myGraph.addEdge("A", "C");
+
+		
+		myGraph.printGraph();
+
 	}
 
 
