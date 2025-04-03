4d8fef0aee22b4ced079a9f56b54fb7009c6fa3f
Marina Leide <marina.msleide@gmail.com>
2025-04-03 20:34:58 -0300

    Implementing Heap class and methods to help with insert method in the future

---
+++ src/Heap/Heap.java
@@ -0,0 +1,35 @@
+package Heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+
+	private List<Integer> heap; 
+	
+	public Heap() {
+		heap = new ArrayList<Integer>();
+	}
+	
+	public List<Integer> getHeap() {
+		return new ArrayList<Integer>(heap);
+	}
+	
+	private int leftChild(int index) {
+		return 2 * index + 1;
+	}
+	
+	private int rightChild(int index) {
+		return  2  * index + 2;
+	}
+	
+	private int parent(int index) {
+		return (index -1) / 2;
+	}
+	
+	private void swap(int index1, int index2) {
+		int temp = heap.get(index1);
+		heap.set(index1, heap.get(index2));
+		heap.set(index2, temp);
+	}
+}
