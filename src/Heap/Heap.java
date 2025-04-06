3e81c17472d656b9f86a190f4e348672858cad69
Marina Leide <marina.msleide@gmail.com>
2025-04-06 11:24:38 -0300

    Implementing sinkDown() method for Heap class

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -43,4 +43,43 @@ public class Heap {
 			
 		}
 	}
+	
+    public Integer remove() {
+        
+        if (heap.size() == 0) return null;
+        if (heap.size() == 1) return heap.remove(0);
+        
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1 ));
+        
+        
+        sinkDown(0);
+        return maxValue;
+    }
+
+	private void sinkDown(int index) {
+		int maxIndex = index;
+		
+		while (true) {
+			int rightIndex = rightChild(index);
+			int leftIndex = leftChild(index);
+			
+			if (leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)) {
+				maxIndex =  leftIndex;
+			}
+			
+			if (rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)) {
+				maxIndex = rightIndex;
+			}
+			
+			if (maxIndex != index) {
+				swap(index, maxIndex);
+				index = maxIndex;
+			} else {
+				return;
+			}
+		}
+	}
+	
+	
 }
