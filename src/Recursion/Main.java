a2edf7b231d9b6198d381dc491ae21cd3f46b39e
Marina Leide <marina.msleide@gmail.com>
2025-05-01 09:08:59 -0300

    Implemeting factorial in a control structure way

--- src/Recursion/Main.java
+++ src/Recursion/Main.java
@@ -4,14 +4,30 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-		int myFatorial = fatorial(10);
-		System.out.println(myFatorial);
+		System.out.println(fatorial(5));
+		
+		System.out.println(factorialWithLoop(5));
 		
 	}
 	
+	//Calculating factorial with recursion
 	public static int fatorial(int n) {
 		if (n == 1) return 1;
 
 		return n * fatorial(n - 1);
+	}
+	
+	//calculating factorial with 
+	
+	public static int factorialWithLoop(int n) {
+		
+		int factorial = 1;
+
+		//factorial of 1 is always 1
+		for (int i = 2 ; i <= n; i ++) {
+			factorial  *= i;
+		}
+		
+		return factorial;
 	}
 }
