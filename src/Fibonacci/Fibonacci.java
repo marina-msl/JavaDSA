8d9ca3991dc381159170d0961131612486415003
Marina Leide <marina.msleide@gmail.com>
2025-05-01 11:28:35 -0300

    Calculating Fibonacci, the most interesting way!

---
+++ src/Fibonacci/Fibonacci.java
@@ -0,0 +1,28 @@
+package Fibonacci;
+
+public class Fibonacci {
+
+	
+	public static void main(String[] args) {
+		//There are three ways of calculating Fibonacci
+		//here is the most interesting way
+		System.out.println(fibonacciCalculate(5));
+	}
+
+	private static int fibonacciCalculate(int n) {
+
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+		
+		int beforePrevious = 0;
+		int previous = 1;
+		int number = 0;
+		
+		for (int i = 2; i <= n; i++) {
+			number = beforePrevious + previous;
+			beforePrevious = previous;
+			previous = number;
+		}
+		return number;
+	}
+}
