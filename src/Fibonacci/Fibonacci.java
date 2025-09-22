c305e6469dae3bcd2bda745465e027b999978c7e
Marina Leide <marina.msleide@gmail.com>
2025-09-22 16:51:41 -0300

    Refactoring on the how to find the base of the Fibonacci method

--- src/Fibonacci/Fibonacci.java
+++ src/Fibonacci/Fibonacci.java
@@ -11,8 +11,7 @@ public class Fibonacci {
 
 	private static int fibonacciCalculate(int n) {
 
-		if (n == 0) return 0;
-		if (n == 1) return 1;
+		if (n == 0 || n == 1) return n;
 		
 		int beforePrevious = 0;
 		int previous = 1;
