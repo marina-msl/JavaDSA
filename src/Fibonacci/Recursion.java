10b72d04e8552366ea5f01aa4cdbd0984b81ab1d
Marina Leide <marina.msleide@gmail.com>
2025-09-22 17:10:04 -0300

    Resolvindo Fibonacci with Recursion

---
+++ src/Fibonacci/Recursion.java
@@ -0,0 +1,18 @@
+package Fibonacci;
+
+public class Recursion {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(fib(7));
+		
+	}
+	
+	public static int fib(int n) {
+		
+		if (n == 0 || n == 1) return n;
+		
+		return fib(n - 1) + fib(n - 2);
+	}
+
+}
