4b66b420f76946a6f8f50a2eeceaede85fec7157
Marina Leide <marina.msleide@gmail.com>
2025-09-22 17:21:13 -0300

    Calculating Fibonacci number with recursion

--- src/Fibonacci/Recursion.java
+++ src/Fibonacci/Recursion.java
@@ -2,14 +2,16 @@ package Fibonacci;
 
 public class Recursion {
 	
+	static int counter = 0 ;
+	
 	public static void main(String[] args) {
 		
-		System.out.println(fib(7));
-		
+		System.out.println(fib(40));
+		System.out.println(counter);
 	}
 	
 	public static int fib(int n) {
-		
+		counter++;
 		if (n == 0 || n == 1) return n;
 		
 		return fib(n - 1) + fib(n - 2);
